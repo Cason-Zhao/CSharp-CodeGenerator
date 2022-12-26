aa3c4bd2b25cb2b3e78272c59dedd00bc47d07c4
刘艳 TvT <2523156074@qq.com>
2022-12-26 14:59:18 +0800

    添加功能，例：进度条、维护连接字符串等

---
+++ CSharp-CodeGenerator2/CSharp-CodeGenerator2/FMConditionSave.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+using System.Collections.Generic;
+
+namespace CSharp_CodeGenerator2
+{
+    partial class FMConditionSave
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.bsData = new System.Windows.Forms.BindingSource(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.cbxConnectStr = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tableNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.bsData)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(93, 9);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "删除";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 9);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // bsData
+            // 
+            this.bsData.DataSource = typeof(System.Collections.Generic.List<CodeGenerator.DBUtil.Models.PreCondition>);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnImport);
+            this.panel2.Controls.Add(this.btnDelete);
+            this.panel2.Controls.Add(this.cbxConnectStr);
+            this.panel2.Controls.Add(this.btnSave);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 63);
+            this.panel2.TabIndex = 3;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(174, 9);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 4;
+            this.btnImport.Text = "导入条件";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // cbxConnectStr
+            // 
+            this.cbxConnectStr.FormattingEnabled = true;
+            this.cbxConnectStr.Location = new System.Drawing.Point(12, 38);
+            this.cbxConnectStr.Name = "cbxConnectStr";
+            this.cbxConnectStr.Size = new System.Drawing.Size(776, 20);
+            this.cbxConnectStr.TabIndex = 3;
+            this.cbxConnectStr.SelectedValueChanged += new System.EventHandler(this.cbxConnectStr_SelectedValueChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.tableNameDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.bsData;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 387);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dataGridView1_RowPostPaint);
+            // 
+            // tableNameDataGridViewTextBoxColumn
+            // 
+            this.tableNameDataGridViewTextBoxColumn.DataPropertyName = "FilterText";
+            this.tableNameDataGridViewTextBoxColumn.HeaderText = "FilterText";
+            this.tableNameDataGridViewTextBoxColumn.Name = "tableNameDataGridViewTextBoxColumn";
+            this.tableNameDataGridViewTextBoxColumn.Width = 500;
+            // 
+            // FMConditionSave
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FMConditionSave";
+            this.Text = "条件维护";
+            this.Load += new System.EventHandler(this.FMConditionSave_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bsData)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource bsData;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox cbxConnectStr;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tableNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button btnImport;
+    }
+}
--- CSharp-CodeGenerator2/CSharp-CodeGenerator2/FMPocoGeneratorY.cs
+++ CSharp-CodeGenerator2/CSharp-CodeGenerator2/FMPocoGeneratorY.cs
@@ -10,6 +10,7 @@ using System.Drawing;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,6 +18,8 @@ namespace CSharp_CodeGenerator2
 {
     public partial class FMPocoGeneratorY : Form
     {
+        #region PropertiesInfo
+
         private List<TableInfo> _TableInfos;
         private bool _IsConnecting
         {
@@ -24,17 +27,31 @@ namespace CSharp_CodeGenerator2
         }
 
         private PreConditions _PreConditions;
+
+        private DateTime _ConnectStartTime;
+        private DateTime _ConnectEndTime;
+        private int _ConnectMilliseconds;
+
+        delegate void AsynUpdateProgressBar(int step);
+        private BackgroundWorker _Worker = new BackgroundWorker();
+        
+        #endregion
+
         public FMPocoGeneratorY()
         {
             InitializeComponent();
 
             txtDataSource.Text = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
             this.Text += $"({Assembly.GetEntryAssembly().GetName().Version.ToString()})";
+        
+            _Worker.WorkerReportsProgress = true;
+            _Worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+            _Worker.RunWorkerCompleted+=new RunWorkerCompletedEventHandler(worker_RunWorkerCompleted);                       
         }
 
         private void FMPocoGeneratorY_Load(object sender, EventArgs e)
         {
-            if(!string.IsNullOrWhiteSpace(this.txtDataSource.Text))
+            if (!string.IsNullOrWhiteSpace(this.txtDataSource.Text))
             {
                 //_PreConditions = 
                 //btnPreCondition.Text = $"预设条件({count})";
@@ -53,6 +70,8 @@ namespace CSharp_CodeGenerator2
             }
         }
 
+        #region gridTables
+
         private void gridTables_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             if (e.RowIndex < 0)
@@ -67,22 +86,10 @@ namespace CSharp_CodeGenerator2
             }
         }
 
-        private void gridColumns_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
-        {
-            if (e.RowIndex < 0)
-            {
-                return;
-            }
-            var bindItem = gridColumns.Rows[e.RowIndex].DataBoundItem as ColumnInfo;
-            if (bindItem != null)
-            {
-                bindItem.IsSelected = !bindItem.IsSelected;
-            }
-            bsColumnInfosSelected.DataSource = _TableInfos.SelectMany(p => p.ColumnInfos.Where(y => y.IsSelected).ToList()).ToList();
-            bsColumnInfosSelected.ResetBindings(true);
+        #endregion
 
-            gridColumns.Refresh();
-        }
+        #region gridColumns
+
         private void gridColumns_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             if (e.RowIndex < 0)
@@ -90,20 +97,21 @@ namespace CSharp_CodeGenerator2
                 return;
             }
 
-            if (e.ColumnIndex == 0)
+            var bindItem = gridColumns.Rows[e.RowIndex].DataBoundItem as ColumnInfo;
+            if (bindItem != null)
             {
-                var bindItem = gridColumns.Rows[e.RowIndex].DataBoundItem as ColumnInfo;
-                if (bindItem != null)
-                {
-                    bindItem.IsSelected = !bindItem.IsSelected;
-                }
-                bsColumnInfosSelected.DataSource = _TableInfos.SelectMany(p => p.ColumnInfos.Where(y => y.IsSelected).ToList()).ToList();
-                bsColumnInfosSelected.ResetBindings(true);
-                gridColumns.Refresh();
+                bindItem.IsSelected = !bindItem.IsSelected;
             }
+            bsColumnInfosSelected.DataSource = _TableInfos.SelectMany(p => p.ColumnInfos.Where(y => y.IsSelected).ToList()).ToList();
+            bsColumnInfosSelected.ResetBindings(true);
+            gridColumns.Refresh();
         }
 
-        private void gridColumnSelected_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        #endregion
+
+        #region gridColumnSelected
+
+        private void gridColumnSelected_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             if (e.RowIndex < 0)
             {
@@ -119,6 +127,8 @@ namespace CSharp_CodeGenerator2
             bsColumnInfosSelected.ResetBindings(true);
             gridColumns.Refresh();
         }
+
+        #endregion
 
         private void btnClearSelected_Click(object sender, EventArgs e)
         {
@@ -141,7 +151,9 @@ namespace CSharp_CodeGenerator2
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
-            SearchData();
+            _ConnectStartTime = DateTime.Now;           
+
+            _Worker.RunWorkerAsync();
         }
 
         private void SearchData()
@@ -155,6 +167,11 @@ namespace CSharp_CodeGenerator2
             {
                 MessageBox.Show("请先连接！", "提示");
                 return;
+            }
+
+            if (!timer1.Enabled)
+            {
+                timer1.Start();
             }
 
             using (var helper = new DBHelper(txtDataSource.Text))
@@ -185,6 +202,13 @@ namespace CSharp_CodeGenerator2
                     return;
                 }
             }
+
+            Action action = BindData;
+            this.Invoke(action);
+        }
+
+        private void BindData()
+        {
             bsData.DataSource = _TableInfos;
             bsData.ResetBindings(true);
             if (_TableInfos.Count > 0)
@@ -194,6 +218,16 @@ namespace CSharp_CodeGenerator2
                 bsColumnInfosSelected.DataSource = _TableInfos.SelectMany(p => p.ColumnInfos.Where(y => y.IsSelected).ToList()).ToList();
                 bsColumnInfosSelected.ResetBindings(true);
             }
+
+            var timeSpan = DateTime.Now - _ConnectStartTime; // 连接后时间
+            var connectMilliseconds = timeSpan.TotalMilliseconds; // 连接所需毫秒数
+
+            var nowConMilliseconds = (connectMilliseconds + (_ConnectMilliseconds == 0 ? _ConnectMilliseconds : _ConnectMilliseconds)) / 2;// 新的平均连接时间
+            PreConditionUtil.SetConnectionMilliseconds(txtDataSource.Text, nowConMilliseconds);
+
+            _ConnectMilliseconds = (int)nowConMilliseconds;
+
+            InitProgressBar();
         }
 
         #region Toggle
@@ -226,7 +260,7 @@ namespace CSharp_CodeGenerator2
                 return;
             }
             var selectedList = _TableInfos.SelectMany(p => p.ColumnInfos.Where(y => y.IsSelected).ToList()).ToList();
-            if(selectedList.Count == 0)
+            if (selectedList.Count == 0)
             {
                 MessageBox.Show("请先选择列信息！");
                 return;
@@ -235,7 +269,8 @@ namespace CSharp_CodeGenerator2
 
             #region Code
 
-            PocoType.GetAllKeys()
+            PocoType pType = new PocoType();
+            pType.GetAllKeys()
                 .ToList()
                 .ForEach(pocoType => GeneratedCodes.Add(pocoType, PocoGenerator.Generate(selectedList, pocoType)));
 
@@ -251,13 +286,19 @@ namespace CSharp_CodeGenerator2
             imgList.ImageSize = new Size(1, 30);// 设置行高 20 //分别是宽和高  
             listVTargetMenu.SmallImageList = imgList; //这里设置listView的SmallImageList ,用imgList将其撑大  
 
-            listVTargetMenu.Items.AddRange(PocoType.GetAllList().Select(p => new ListViewItem(p.Value)
+            listVTargetMenu.Items.AddRange(pType.GetAllList().Select(p => new ListViewItem(p.Value)
             {
                 Name = $"A{p.Key}",
                 Tag = p.Key,
             }).ToArray());
 
             listVTargetMenu.EndUpdate();
+
+            if (listVTargetMenu.Items.Count > 0)
+            {
+                this.listVTargetMenu.Items[0].Selected = true;
+                this.listVTargetMenu.Select();
+            }
 
             #endregion
 
@@ -270,10 +311,10 @@ namespace CSharp_CodeGenerator2
 
         private void listVTargetMenu_Click(object sender, EventArgs e)
         {
-            if(GeneratedCodes.Count > 0)
+            if (GeneratedCodes.Count > 0)
             {
                 var selectedItem = listVTargetMenu.Items.OfType<ListViewItem>().Where(p => p.Focused).FirstOrDefault();
-                if(selectedItem != null)
+                if (selectedItem != null)
                 {
                     var pocoType = selectedItem.Tag.ToString();
                     richTxtTarget.Text = !GeneratedCodes.ContainsKey(pocoType) ? "" : GeneratedCodes[pocoType];
@@ -284,12 +325,12 @@ namespace CSharp_CodeGenerator2
         // 预设条件
         private void btnPreCondition_Click(object sender, EventArgs e)
         {
-            if(string.IsNullOrEmpty(this.txtDataSource.Text))
+            if (string.IsNullOrEmpty(this.txtDataSource.Text))
             {
                 MessageBox.Show("请先填入连接字符串");
                 return;
             }
-            if(!_IsConnecting)
+            if (!_IsConnecting)
             {
                 MessageBox.Show("请先连接！", "提示");
                 return;
@@ -306,16 +347,21 @@ namespace CSharp_CodeGenerator2
 
         private void btnConnect_Click(object sender, EventArgs e)
         {
-            if(string.IsNullOrEmpty(txtDataSource.Text))
+            _ConnectStartTime = DateTime.Now;
+
+            if (string.IsNullOrEmpty(txtDataSource.Text))
             {
                 MessageBox.Show("请先填入连接字符串!");
                 return;
             }
 
-            if(!_IsConnecting)
+            if (!_IsConnecting)
             {
                 try
                 {
+                    _ConnectMilliseconds = (int)PreConditionUtil.GetConnectionMilliseconds(txtDataSource.Text); // 之前所有连接的平均时间
+                    InitProgressBar();
+
                     using (SqlConnection objConnection = new SqlConnection(txtDataSource.Text))
                     {
                         objConnection.Open();
@@ -346,7 +392,8 @@ namespace CSharp_CodeGenerator2
             // 加载数据
             if (_IsConnecting)
             {
-                SearchData();
+                _Worker.RunWorkerAsync();
+                timer1.Start();
             }
             else// 清理数据
             {
@@ -359,6 +406,82 @@ namespace CSharp_CodeGenerator2
                 bsColumnInfosSelected.DataSource = null;
                 bsColumnInfosSelected.ResetBindings(true);
             }
+        }
+
+        // 排序
+        private void gridTables_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            SortTable(gridTables, e);
+        }
+
+        // 排序
+        private void gridColumns_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            SortTable(gridColumns, e);
+        }
+
+        // 排序
+        private void gridColumnSelected_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            SortTable(gridColumnSelected, e);
+        }
+
+        // 排序
+        private void SortTable(DataGridView dataGridView, DataGridViewRowPostPaintEventArgs e)
+        {
+            //自动编号，与数据无关
+            Rectangle rectangle = new Rectangle(e.RowBounds.Location.X,
+               e.RowBounds.Location.Y,
+               dataGridView.RowHeadersWidth - 4,
+               e.RowBounds.Height);
+            TextRenderer.DrawText(e.Graphics,
+                  (e.RowIndex + 1).ToString(),
+                   dataGridView.RowHeadersDefaultCellStyle.Font,
+                   rectangle,
+                   dataGridView.RowHeadersDefaultCellStyle.ForeColor,
+                   TextFormatFlags.VerticalCenter | TextFormatFlags.Right);
+        }
+
+        #region 进度条
+
+        private void InitProgressBar()
+        {
+            progressBar.Maximum = _ConnectMilliseconds == 0 ? 100 : _ConnectMilliseconds; ;
+            progressBar.Step = progressBar.Maximum / 100;
+            progressBar.Value = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar.Value < progressBar.Maximum)
+            {
+                progressBar.PerformStep();
+                lblProgressBar.Text = (progressBar.Value * 100 / progressBar.Maximum).ToString() + "%";
+            }
+            else
+            {
+                lblProgressBar.Text = "99%";
+            }
+        }
+
+        void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            timer1.Stop();
+            this.progressBar.Value = this.progressBar.Maximum;
+            lblProgressBar.Text = "连接完成";
+        }
+
+        void worker_DoWork(object sernder, DoWorkEventArgs e)
+        {
+            SearchData();
+        }
+
+        #endregion
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            FMConditionSave form = new FMConditionSave();
+            form.Show();
         }
     }
 }
